fcf2f2395b9c8e60f64e25503c68792a1d6753a5
Tucker Wilkes <twilkes149@gmail.com>
2018-09-11 15:08:10 -0600

    started perceptron

---
+++ src/Perceptron/Perceptron.java
@@ -0,0 +1,20 @@
+package Perceptron;
+import Perceptron.Neuron;
+import Default.Matrix;
+import Default.SupervisedLearner;
+
+public class Perceptron extends SupervisedLearner {
+
+	@Override
+	public void train(Matrix features, Matrix labels) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void predict(double[] features, double[] labels) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
